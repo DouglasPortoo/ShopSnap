5f56e1d4c330025fbbe7f562fdad6aa532b66e02
Douglas Porto <portogdouglas@gmail.com>
2024-10-07 11:04:16 -0300

    Add initial project files

---
+++ src/main/java/com/douglasporto/ShopSnap/ShopSnapApplication.java
@@ -0,0 +1,13 @@
+package com.douglasporto.ShopSnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglasporto/ShopSnap/ShopSnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasporto.ShopSnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
