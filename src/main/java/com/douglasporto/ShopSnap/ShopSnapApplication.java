3fea8807b95a459782a4a9aa22f40f87efbced17
Douglas Porto <portogdouglas@gmail.com>
2024-10-07 16:21:21 -0300

    Product and many-to-many association

--- src/main/java/com/douglasporto/ShopSnap/ShopSnapApplication.java
+++ src/main/java/com/douglasporto/ShopSnap/ShopSnapApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.douglasporto.ShopSnap.domain.Categoria;
+import com.douglasporto.ShopSnap.domain.Produto;
 import com.douglasporto.ShopSnap.repositories.CategoriaRepository;
+import com.douglasporto.ShopSnap.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ShopSnapApplication implements CommandLineRunner {
@@ -16,16 +18,32 @@ public class ShopSnapApplication implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShopSnapApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 
