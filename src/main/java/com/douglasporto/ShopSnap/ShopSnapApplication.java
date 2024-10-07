9e3a44114abdc3506519f2a16c3c003ee91c5af5
Douglas Porto <portogdouglas@gmail.com>
2024-10-07 15:12:06 -0300

    Creating instantiation operation

--- src/main/java/com/douglasporto/ShopSnap/ShopSnapApplication.java
+++ src/main/java/com/douglasporto/ShopSnap/ShopSnapApplication.java
@@ -1,13 +1,32 @@
 package com.douglasporto.ShopSnap;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.douglasporto.ShopSnap.domain.Categoria;
+import com.douglasporto.ShopSnap.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ShopSnapApplication {
+public class ShopSnapApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopSnapApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
