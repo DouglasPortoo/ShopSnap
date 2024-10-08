75ee123d88f5ab3ca46f667177b8236517b176e5
Douglas Porto <portogdouglas@gmail.com>
2024-10-08 11:24:22 -0300

    Add instantiation operation for Cliente and Endereco entities

--- src/main/java/com/douglasporto/ShopSnap/ShopSnapApplication.java
+++ src/main/java/com/douglasporto/ShopSnap/ShopSnapApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.douglasporto.ShopSnap.domain.Categoria;
 import com.douglasporto.ShopSnap.domain.Cidade;
+import com.douglasporto.ShopSnap.domain.Cliente;
+import com.douglasporto.ShopSnap.domain.Endereco;
 import com.douglasporto.ShopSnap.domain.Estado;
 import com.douglasporto.ShopSnap.domain.Produto;
+import com.douglasporto.ShopSnap.domain.enums.TipoCliente;
 import com.douglasporto.ShopSnap.repositories.CategoriaRepository;
 import com.douglasporto.ShopSnap.repositories.CidadeRepository;
+import com.douglasporto.ShopSnap.repositories.ClienteRepository;
+import com.douglasporto.ShopSnap.repositories.EnderecoRepository;
 import com.douglasporto.ShopSnap.repositories.EstadoRepository;
 import com.douglasporto.ShopSnap.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class ShopSnapApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopSnapApplication.class, args);
@@ -62,10 +73,23 @@ public class ShopSnapApplication implements CommandLineRunner {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
 
 	}
 
