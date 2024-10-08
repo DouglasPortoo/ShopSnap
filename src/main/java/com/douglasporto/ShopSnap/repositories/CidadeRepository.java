5a9cb85480ff097eead5b2783c6986bfb12395e0
Douglas Porto <portogdouglas@gmail.com>
2024-10-08 10:18:35 -0300

    Add instantiation operation for Estado and Cidade entities

---
+++ src/main/java/com/douglasporto/ShopSnap/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.douglasporto.ShopSnap.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.douglasporto.ShopSnap.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+  
+}
