aa16749df7f0a2ac48df9846e97c2483e4d12c7d
Douglas Porto <portogdouglas@gmail.com>
2024-10-09 09:44:36 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/douglasporto/ShopSnap/resources/PedidoResource.java
+++ src/main/java/com/douglasporto/ShopSnap/resources/PedidoResource.java
@@ -18,9 +18,9 @@ public class PedidoResource {
   private PedidoService service;
 
   @GetMapping(value = "/{id}")
-  public ResponseEntity<?> find(@PathVariable Integer id) {
+  public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 
-    Pedido obj = service.buscaDados(id);
+    Pedido obj = service.find(id);
     return ResponseEntity.ok().body(obj);
   }
 
