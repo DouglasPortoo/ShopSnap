a81cc12f7ccde092cb08ad3ce2d40a55cfb73eb5
Douglas Porto <portogdouglas@gmail.com>
2024-10-09 14:23:05 -0300

    Personalizando o retorno da validação

--- src/main/java/com/douglasporto/ShopSnap/resources/handlers/ResourcesExceptionHandler.java
+++ src/main/java/com/douglasporto/ShopSnap/resources/handlers/ResourcesExceptionHandler.java
@@ -2,11 +2,14 @@ package com.douglasporto.ShopSnap.resources.handlers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.douglasporto.ShopSnap.services.exceptions.ObjectNotFoundException;
 import com.douglasporto.ShopSnap.services.exceptions.DataIntegrityException;
+
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -22,6 +25,17 @@ public class ResourcesExceptionHandler {
   @ExceptionHandler(DataIntegrityException.class)
   public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
     StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+    ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+
+    for(FieldError x : e.getBindingResult().getFieldErrors()) {
+      err.addError(x.getField(), x.getDefaultMessage());
+    }
+
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
   }
 }
