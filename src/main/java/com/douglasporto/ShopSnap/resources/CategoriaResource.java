e9c541fdbb9179beaf609f2d01915504e5d3097b
Douglas Porto <portogdouglas@gmail.com>
2024-10-09 09:29:28 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/douglasporto/ShopSnap/resources/CategoriaResource.java
+++ src/main/java/com/douglasporto/ShopSnap/resources/CategoriaResource.java
@@ -2,14 +2,19 @@ package com.douglasporto.ShopSnap.resources;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.douglasporto.ShopSnap.domain.Categoria;
 import com.douglasporto.ShopSnap.services.CategoriaService;
+
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/categorias")
@@ -23,5 +28,13 @@ public class CategoriaResource {
 
     Categoria obj = service.buscaDados(id);
     return ResponseEntity.ok().body(obj);
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+    obj = service.insert(obj);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+      .path("/{id}").buildAndExpand(obj.getId()).toUri();
+    return ResponseEntity.created(uri).build();
   }
 }
