3c573f9c405ef7896ddd3f34ced2c6dc8aab1e25
Douglas Porto <portogdouglas@gmail.com>
2024-10-14 14:13:07 -0300

    Email HTML

--- src/main/java/com/douglasporto/ShopSnap/services/IEmailService.java
+++ src/main/java/com/douglasporto/ShopSnap/services/IEmailService.java
@@ -4,7 +4,14 @@ import org.springframework.mail.SimpleMailMessage;
 
 import com.douglasporto.ShopSnap.domain.Pedido;
 
+import jakarta.mail.internet.MimeMessage;
+
 public interface IEmailService {
   void sendOrderConfirmationEmail(Pedido obj);
+
   void sendEmail(SimpleMailMessage msg);
+
+  void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+  void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/douglasporto/ShopSnap/services/SmtpEmailService.java
+++ src/main/java/com/douglasporto/ShopSnap/services/SmtpEmailService.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import jakarta.mail.internet.MimeMessage;
 
 public class SmtpEmailService extends AbstractEmailService {
 
@@ -12,6 +15,9 @@ public class SmtpEmailService extends AbstractEmailService {
 
   @Autowired
   private MailSender mailSender;
+
+  @Autowired
+  private JavaMailSender javaMailSender;
 
   @Override
   public void sendEmail(SimpleMailMessage msg) {
@@ -21,5 +27,14 @@ public class SmtpEmailService extends AbstractEmailService {
     LOG.info("Email enviado!");
 
     mailSender.send(msg);
+  }
+
+  @Override
+  public void sendHtmlEmail(MimeMessage msg) {
+    LOG.info("Enviando...");
+    LOG.info(msg.toString());
+    LOG.info("Email enviado!");
+
+    javaMailSender.send(msg);
   }
 }
